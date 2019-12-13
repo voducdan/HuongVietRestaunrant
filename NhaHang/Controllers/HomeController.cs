99637ceead8e49171517ce78cdc5e259423a5dac
voducdan <voducdand99@gmail.com>
2019-12-14 01:58:54 +0700

    connect db && show list dishes

--- NhaHang/Controllers/HomeController.cs
+++ NhaHang/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using NhaHang.Models;
 namespace NhaHang.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            NhaHangEntities NhaHang = new NhaHangEntities();
+            MonAnQuery ListMonAn = new MonAnQuery();
+            ListMonAn.MonAnList = (from MA in NhaHang.MonAns
+                             join LM in NhaHang.LoaiMons on MA.Idloaimon equals LM.Idloaimon
+                             select new MonAnQuery()
+                             {
+                                 TenMon = MA.Tenmon,
+                                 Gia = MA.Gia,
+                                 LoaiMon = LM.Tenloaimon,
+                             }).Take(10000).ToList();
+            return View(ListMonAn);
         }
     }
 }
---
+++ NhaHang/Models/MonAnQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NhaHang.Models
+{
+    public class MonAnQuery
+    {
+        public string TenMon { get; set; }
+        public double? Gia { get; set; }
+        public string LoaiMon { get; set; }
+        public IList<MonAnQuery> MonAnList{get;set;}
+}
+}
